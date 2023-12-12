3ec2bfc6d43b5309ceebddb61b1d9cd0542c5366
heqile <mr.qile@gmail.com>
2023-12-12 21:26:19 +0100

    feat: read command line

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 #include <regex>
 #include <vector>
 
-
 using namespace std;
 
 struct VersionDetail
@@ -14,12 +13,14 @@ struct VersionDetail
     vector<string> details;
 };
 
-vector<int> splitStringToInt(const string& data, const string& delimiter) noexcept {
+vector<int> split_string_to_int(const string &data, const string &delimiter) noexcept
+{
     vector<int> result;
     auto prepos = 0;
     auto i = data.find(delimiter);
-    while (i != -1) {
-        const auto&& part = data.substr(prepos, i);
+    while (i != -1)
+    {
+        const auto &&part = data.substr(prepos, i);
         result.push_back(stoi(part));
         prepos = i + delimiter.size();
         i = data.find(delimiter, prepos);
@@ -31,36 +32,43 @@ struct ParsingContext
 {
     vector<unique_ptr<VersionDetail>> vd;
 
-    void add_line(const string& line) noexcept {
-        if (line.empty()) {
+    void add_line(const string &line) noexcept
+    {
+        if (line.empty())
+        {
             return;
         }
         regex reg("\\d+\\.\\d+\\.\\d+");
-        if (regex_match(line, reg)) {
+        if (regex_match(line, reg))
+        {
             vd.push_back(unique_ptr<VersionDetail>(new VersionDetail));
             vd.back()->tag = line;
             return;
         }
-        if (!line.starts_with("-")) {
+        if (!line.starts_with("-"))
+        {
             return;
         }
-        if (vd.size() == 0) {
+        if (vd.size() == 0)
+        {
             return;
         }
-        auto& current = vd.back();
+        auto &current = vd.back();
         current->details.push_back(line);
         return;
     };
 
-    string serialize() noexcept {
+    string serialize() noexcept
+    {
         string result;
-        sort(vd.begin(), vd.end(), [](const unique_ptr<VersionDetail>& vd1, const unique_ptr<VersionDetail>& vd2){
-            return splitStringToInt(vd1->tag, ".") > splitStringToInt(vd2->tag, ".");
-        });
-        for (const auto& i: vd) {
+        sort(vd.begin(), vd.end(), [](const unique_ptr<VersionDetail> &vd1, const unique_ptr<VersionDetail> &vd2)
+             { return split_string_to_int(vd1->tag, ".") > split_string_to_int(vd2->tag, "."); });
+        for (const auto &i : vd)
+        {
             result.append(i->tag);
             result.append("\n");
-            sort(i->details.begin(), i->details.end(), [](const string& s1, const string& s2){
+            sort(i->details.begin(), i->details.end(), [](const string &s1, const string &s2)
+                 {
                 if (s1.starts_with("- fix") && !s1.starts_with("- fix")) {
                     return true;
                 } 
@@ -70,9 +78,9 @@ struct ParsingContext
                 else if (s1.starts_with("- chore") && !s1.starts_with("- chore")) {
                     return true;
                 }
-                return s1 < s2;
-            });
-            for (const auto& j: i->details) {
+                return s1 < s2; });
+            for (const auto &j : i->details)
+            {
                 result.append(j);
                 result.append("\n");
             }
@@ -82,9 +90,10 @@ struct ParsingContext
     }
 };
 
-
-string trimmed(string& s) {
-    auto is_not_space = [](unsigned char ch){
+string trimmed(string &s)
+{
+    auto is_not_space = [](unsigned char ch)
+    {
         return !isspace(ch);
     };
     s.erase(s.begin(), find_if(s.begin(), s.end(), is_not_space));
@@ -92,16 +101,30 @@ string trimmed(string& s) {
     return s;
 }
 
-int main(int argc, char* argv[]) {
-    auto file = fstream("/home/qile/playground/change_log_checker/data.txt");
-    if (file.is_open()) {
-        string line;
-        ParsingContext ctx;
-        while (getline(file, line)) {
-            line = trimmed(line);
-            ctx.add_line(line);
-        }
-        file.close();
-        cout<<ctx.serialize()<<endl;
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        std::cout << "Usage: change_log_checker [file path]" << std::endl;
+        exit(0);
     }
+
+    auto file_path = argv[1];
+    auto file = fstream(file_path);
+    if (!file.is_open())
+    {
+        std::cout << "Can not open file: " << file_path << std::endl;
+        exit(1);
+    }
+
+    string line;
+    ParsingContext ctx;
+    while (getline(file, line))
+    {
+        line = trimmed(line);
+        ctx.add_line(line);
+    }
+    file.close();
+    std::cout << ctx.serialize() << std::endl;
+    exit(0);
 }
