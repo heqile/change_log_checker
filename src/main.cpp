4a442b8cd378f26ab44a18ca75d2ed6e26a297de
heqile <mr.qile@gmail.com>
2023-12-21 22:09:10 +0100

    refactor: make ParsingContext class

--- src/main.cpp
+++ src/main.cpp
@@ -13,13 +13,36 @@ struct VersionDetail
     vector<pair<int, string>> details;
 };
 
-struct ParsingContext
+struct ParsingContextConfiguration
 {
-    string _tag_prefix;
-    string _item_prefix;
-    vector<string> _order;
-    vector<shared_ptr<VersionDetail>> vertion_detail{};
+    const string tag_prefix;
+    const string item_prefix;
+    const vector<string> order;
+};
+
+class ParsingContext
+{
+  private:
+    const ParsingContextConfiguration _config;
+    const regex _tag_reg;
+    const regex _item_reg;
+    vector<shared_ptr<VersionDetail>> _vertion_detail{};
     shared_ptr<VersionDetail> _current_vertion_detail{nullptr};
+
+    static regex _get_tag_reg(const ParsingContextConfiguration &config) noexcept
+    {
+        return regex(config.tag_prefix.empty() ? "(\\d+)\\.(\\d+)\\.(\\d+)"
+                                               : config.tag_prefix + "\\s*(\\d+)\\.(\\d+)\\.(\\d+)");
+    };
+
+    static regex _get_item_reg(const ParsingContextConfiguration &config) noexcept
+    {
+        return regex(config.item_prefix.empty() ? "\\s*(.+)\\s*$" : config.item_prefix + "\\s*(.+)\\s*$");
+    };
+
+  public:
+    ParsingContext(const ParsingContextConfiguration &config) noexcept
+        : _config{config}, _tag_reg{_get_tag_reg(config)}, _item_reg{_get_item_reg(config)} {};
 
     void add_line(const string &line) noexcept
     {
@@ -28,18 +51,15 @@ struct ParsingContext
             return;
         }
 
-        const regex tag_reg{_tag_prefix.empty() ? "(\\d+)\\.(\\d+)\\.(\\d+)"
-                                                : _tag_prefix + "\\s*(\\d+)\\.(\\d+)\\.(\\d+)"};
-
         std::smatch match;
-        if (regex_match(line, match, tag_reg))
+        if (regex_match(line, match, _tag_reg))
         {
             // append new detail object with version tag
             _current_vertion_detail = make_shared<VersionDetail>();
             _current_vertion_detail->tag = tuple<int, int, int>{stoi(match[1]), stoi(match[2]), stoi(match[3])};
-            vertion_detail.push_back(_current_vertion_detail);
+            _vertion_detail.push_back(_current_vertion_detail);
             // sort versions
-            sort(vertion_detail.begin(), vertion_detail.end(),
+            sort(_vertion_detail.begin(), _vertion_detail.end(),
                  [](const shared_ptr<VersionDetail> &vd1, const shared_ptr<VersionDetail> &vd2) {
                      return vd1->tag > vd2->tag;
                  });
@@ -51,19 +71,19 @@ struct ParsingContext
             return;
         }
         std::smatch item_match;
-        const regex item_reg{_item_prefix.empty() ? "\\s*(.+)\\s*$" : _item_prefix + "\\s*(.+)\\s*$"};
-        if (!regex_match(line, item_match, item_reg))
+
+        if (!regex_match(line, item_match, _item_reg))
         {
             return;
         }
         // append detail
         const string &detail = item_match[1];
         // sort details
-        using order_vec_t = decltype(_order);
-        order_vec_t::size_type weight = _order.size();
-        for (order_vec_t::size_type i = 0; i < _order.size(); i++)
+        using order_vec_t = decltype(_config.order);
+        order_vec_t::size_type weight, order_list_size = _config.order.size();
+        for (order_vec_t::size_type i = 0; i < order_list_size; i++)
         {
-            if (detail.starts_with(_order[i]))
+            if (detail.starts_with(_config.order[i]))
             {
                 weight = i;
                 break;
@@ -72,24 +92,24 @@ struct ParsingContext
         _current_vertion_detail->details.push_back(pair{weight, detail});
         sort(begin(_current_vertion_detail->details), end(_current_vertion_detail->details));
         return;
-    }
+    };
 
     [[nodiscard]] auto serialize() const noexcept -> string
     {
         string result;
-        for (const auto &i : vertion_detail)
+        for (const auto &i : _vertion_detail)
         {
             const auto [major, minor, patch] = i->tag;
             auto &&tag = to_string(major) + "." + to_string(minor) + "." + to_string(patch);
-            result += _tag_prefix.empty() ? tag + "\n" : _tag_prefix + " " + tag + "\n";
+            result += _config.tag_prefix.empty() ? tag + "\n" : _config.tag_prefix + " " + tag + "\n";
             for (const auto &j : i->details)
             {
-                result += _item_prefix.empty() ? j.second + "\n" : _item_prefix + " " + j.second + "\n";
+                result += _config.item_prefix.empty() ? j.second + "\n" : _config.item_prefix + " " + j.second + "\n";
             }
             result += "\n";
         }
         return result;
-    }
+    };
 };
 
 auto main(int argc, char *argv[]) -> int
@@ -108,8 +128,9 @@ auto main(int argc, char *argv[]) -> int
         return 1;
     }
 
+    ParsingContextConfiguration config{"####", "-", {"fix", "feat", "chore"}};
+    ParsingContext ctx(config);
     string line;
-    ParsingContext ctx{"####", "-", {"fix", "feat", "chore"}};
     while (getline(file, line))
     {
         ctx.add_line(line);
@@ -117,4 +138,4 @@ auto main(int argc, char *argv[]) -> int
     file.close();
     std::cout << ctx.serialize();
     return 0;
-}
+};
